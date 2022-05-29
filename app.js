ae64fd3b34a047c13646866e11adb5b4131668c4
Nick Cunningham <nic1571@gmail.com>
2022-05-29 11:04:31 -0600

    Create Dinosaur constructor

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
     // Create Dino Constructor
-
+function Dinosaur(species, weight, height, diet, where, when, fact) {
+    this.species = species
+    this.weight = weight,
+    this.height = height,
+    this.diet = diet,
+    this.where = where,
+    this.when = when,
+    this.fact = fact
+}
 
     // Create Dino Objects
 
