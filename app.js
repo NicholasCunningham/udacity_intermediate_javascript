fbc7160b39f1f5a472633f20b1f4ff8345c46176
Nick Cunningham <nic1571@gmail.com>
2022-06-08 12:33:31 -0600

    Add missing closing paren

--- app.js
+++ app.js
@@ -45,5 +45,5 @@ let element = document.querySelector('#diet');
 element.addEventListener('change', (event) => {
     const testSelect = document.querySelector('.testSelect');
     testSelect.textContent = `You selected ${event.target.value}`
-}
+})
 // }
